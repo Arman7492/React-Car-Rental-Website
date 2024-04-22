57173a903b810326ce222a98d2c56ccc1b5a3787
Arman7492 <amuhamadiev7492@gmail.com>
2024-04-22 21:38:40 +0500

    first commit unfinished react project

---
+++ src/assets/data/blogData.js
@@ -0,0 +1,114 @@
+// import images from all-images/blog-img directory
+import axios from "axios";
+import img01 from "../all-images/blog-img/blog-1.jpg";
+import img02 from "../all-images/blog-img/blog-2.jpg";
+import img03 from "../all-images/blog-img/blog-3.jpg";
+
+axios.get('http://rentandcourses.loc/api/orderItems')
+  .then(function (response) {
+    // обработка успешного запроса
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // обработка ошибки
+    console.log(error);
+  })
+  .finally(function () {
+    // выполняется всегда
+  });
+
+axios.get('http://rentandcourses.loc/api/instructors')
+  .then(function (response) {
+    // обработка успешного запроса
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // обработка ошибки
+    console.log(error);
+  })
+  .finally(function () {
+    // выполняется всегда
+  });
+
+  axios.get('http://rentandcourses.loc/api/showOrderItems/{id}')
+  .then(function (response) {
+    // обработка успешного запроса
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // обработка ошибки
+    console.log(error);
+  })
+  .finally(function () {
+    // выполняется всегда
+  });
+
+  axios.get('http://rentandcourses.loc/api/updateOrderItems/{id}')
+  .then(function (response) {
+    // обработка успешного запроса
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // обработка ошибки
+    console.log(error);
+  })
+  .finally(function () {
+    // выполняется всегда
+  });
+
+  axios.get('http://rentandcourses.loc/api/deleteOrderItems/{id}')
+  .then(function (response) {
+    // обработка успешного запроса
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // обработка ошибки
+    console.log(error);
+  })
+  .finally(function () {
+    // выполняется всегда
+  });
+
+
+const blogData = [
+  {
+    id: 1,
+    title: "The best way to drive cars",
+    author: "Muhib",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img01,
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    quote:
+      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+  },
+
+  {
+    id: 2,
+    title: "If your car battery is down",
+    author: "Muhib",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img02,
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    quote:
+      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+  },
+
+  {
+    id: 3,
+    title: "The best way to give trip",
+    author: "Muhib",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img03,
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    quote:
+      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+  },
+];
+
+export default blogData;
